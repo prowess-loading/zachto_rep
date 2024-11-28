1cb88ee26a4496bdb22c496a436fbd4cfe7a0b8c
dramani_bynder <138667640+dramani-bynder@users.noreply.github.com>
2024-11-28 19:00:27 +0530

    fix added in run file

--- run_term.py
+++ run_term.py
@@ -1,5 +1,6 @@
 import platform
 import subprocess
+import os
 
 
 def main():
@@ -9,19 +10,33 @@ def main():
     )
 
     # Prepare the command to execute
-    command = f"python main.py {num_repetition}"
-    is_windows = platform.system() == "Windows"
+    system_platform = platform.system()
+    working_directory = os.getcwd()
 
     for i in range(num_terminals):
-        if is_windows:
+        if system_platform == "Windows":
+            command = f"python main.py {num_repetition}"
             subprocess.Popen(
                 ["cmd", "/c", f"start cmd /c {command}"], shell=True
                 # ["cmd", "/c", f"start cmd /k {command}"], shell=True
             )
+
+        elif system_platform == "Darwin":
+            command = f"python3 main.py {num_repetition}"
+            apple_script = f'''
+            tell application "Terminal"
+                do script "cd {working_directory} && {command}; exit"
+                delay 0.5 -- Ensure the command starts before we proceed
+                tell application "System Events" to keystroke "w" using command down
+            end tell
+            '''
+            subprocess.Popen(["osascript", "-e", apple_script])
+
         else:
+            command = f"python3 main.py {num_repetition}"
             subprocess.Popen(
                 ["gnome-terminal", "--", "bash", "-c",
-                 f'{command}; exec bash']
+                 f'{command}; exit; exec bash']
             )
 
 
